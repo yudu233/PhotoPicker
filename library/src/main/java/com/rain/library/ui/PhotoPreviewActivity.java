69c6368d974f339dc130bbb2c03c7d43b8ff1ca0
duyu <bossrain99@163.com>
2019-08-23 05:07:37 +0800

    index ++

--- library/src/main/java/com/rain/library/ui/PhotoPreviewActivity.java
+++ library/src/main/java/com/rain/library/ui/PhotoPreviewActivity.java
@@ -309,9 +309,9 @@ public class PhotoPreviewActivity extends BaseActivity implements OnPhotoTapList
                     finish();
                 }
             } else {
-                index++;
                 MediaData photo = selectPhotosInfo.get(index);
                 photo.setCompressionPath(photo.getOriginalPath());
+                index++;
             }
         }
     };
